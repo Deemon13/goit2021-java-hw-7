8aa53051aa2b6b6fb48bd581a2cf7e3d46893ce8
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-04-11 22:27:20 +0300

    added interface, 5 classes & class for output

---
+++ src/main/java/FigureName.java
@@ -0,0 +1,18 @@
+public class FigureName {
+    public static void main(String[] args) {
+        Shape circle = new Circle();
+        circle.getName();
+
+        Shape quad = new Quad();
+        quad.getName();
+
+        Shape rectangle = new Rectangle();
+        rectangle.getName();
+
+        Shape rhombus = new Rhombus();
+        rhombus.getName();
+
+        Shape triangle = new Triangle();
+        triangle.getName();
+    }
+}
---
+++ src/main/java/Quad.java
@@ -0,0 +1,6 @@
+public class Quad implements Shape{
+    @Override
+    public void getName() {
+        System.out.println("FigureName: Quad");
+    }
+}
